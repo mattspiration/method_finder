852b68d4f7f9146e7595a4c26e131edc39690664
mattspiration <mpreston@inspirecleanenergy.com>
2023-09-01 10:36:59 -0400

    fixed duplicated method names in search list

--- method_finder.py
+++ method_finder.py
@@ -5,8 +5,8 @@ import time
 import pandas as pd
 
 # List of methods to find
-methods_to_find = ['upload_file_from_param_to_s3', 'upload_file_from_url_to_s3', 'upload_file_from_url_to_s3', 
-                   'upload_local_file_to_s3', 'archive_file_to_s3', 'archive_file_to_s3', 'download_file_from_s3',
+methods_to_find = ['upload_file_from_param_to_s3', 'upload_file_from_url_to_s3',
+                   'upload_local_file_to_s3', 'archive_file_to_s3', 'download_file_from_s3',
                    'get_s3_public_url', 'get_s3_url']
 
 # Initialize an empty DataFrame
@@ -34,7 +34,7 @@ def determine_search_length(dir_path, file_type):
     return (dir_length, file_length)
 
 num_dir, num_files = determine_search_length(start_dir, '.rb')
-print(f'Will search {num_dir} directories and {num_files} files.')
+print(f'Will search {num_dir} directories and {num_files} .rb files.')
 
 # Traverse the file system
 results = []
@@ -43,24 +43,25 @@ for root, dirs, files in os.walk(start_dir):
     tmp_dict = {}
     for file in files:
         files_searched += 1
-        if file.endswith('.rb'):  # Assuming we are looking for Python files
+        if file.endswith('.rb'):  # Assuming we are looking for Ruby files
             with open(os.path.join(root, file), 'r') as f:
                 lines = f.readlines()
                 for i, line in enumerate(lines):
                     for method in methods_to_find:
-                        # Store the information in the DataFrame
-                        tmp_dict['File Name'] = file
-                        tmp_dict['Method Found'] = method
-                        tmp_dict['Line Number'] = i + 1
-                        tmp_dict['Parent Directory'] = root
-                        results.append(tmp_dict)
-                        tmp_dict = {}
+                        if method in line:
+                            # Store the information in the temp_dict and result list
+                            tmp_dict['File Name'] = file
+                            tmp_dict['Method Found'] = method
+                            tmp_dict['Line Number'] = i + 1
+                            tmp_dict['Parent Directory'] = root
+                            results.append(tmp_dict)
+                            tmp_dict = {}
 
 
 # Add the results to the DataFrame
 df = pd.concat([df, pd.DataFrame(results)], ignore_index=True)
 
-
+csv_name = f'{start_dir}_method_references.csv'
 # Create a CSV of the DataFrame
-df.to_csv('method_references_s3.csv', index=False)
+df.to_csv("results.csv", index=False)
 print(f'Files Searched: {files_searched}')
